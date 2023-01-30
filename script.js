e6feb5ab33d0c2b1f81b6b622ba87fcb17efa13f
Shraddha-Tiwari <shraddha9tiwari@gmail.com>
2023-01-30 12:25:41 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,54 @@
+const COLOR_BTNS = document.querySelectorAll('.color');
+COLOR_BTNS.forEach(color => {
+    color.addEventListener('click', () => {
+        let colorNameClass = color.className;
+        if(!color.classList.contains('active-color')){
+            let colorName = colorNameClass.slice(colorNameClass.indexOf('-') + 1, colorNameClass.length);
+            resetActiveBtns();
+            color.classList.add('active-color');
+            setNewColor(colorName)
+        }
+    });
+})
+
+// resetting all color btns
+function resetActiveBtns(){
+    COLOR_BTNS.forEach(color => {
+        color.classList.remove('active-color');
+    });
+}
+
+// set new color on the banner right 
+function setNewColor(color){
+    document.querySelector('.left img').src = `assests/umbrella_${color}.png`;
+}
+
+
+//for company brand
+const selectImage = document.querySelector('.logo');
+const inputFile = document.querySelector('#file');
+const imgArea = document.querySelector('.img-area');
+
+selectImage.addEventListener('click', function () {
+	inputFile.click();
+})
+
+inputFile.addEventListener('change', function () {
+	const image = this.files[0]
+	if(image.size < 5000000) {
+		const reader = new FileReader();
+		reader.onload = ()=> {
+			const allImg = imgArea.querySelectorAll('img');
+			allImg.forEach(item=> item.remove());
+			const imgUrl = reader.result;
+			const img = document.createElement('img');
+			img.src = imgUrl;
+			imgArea.appendChild(img);
+			imgArea.classList.add('active');
+			imgArea.dataset.img = image.name;
+		}
+		reader.readAsDataURL(image);
+	} else {
+		alert("Image size more than 5MB");
+	}
+})
